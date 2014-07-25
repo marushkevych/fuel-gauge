1cd5c6078466fdd4d22d12e6c6e0c6334bbbfb9a
marushkevych <marushkevych@gmail.com>
2014-07-25 10:00:42 -0400

    controller scope

--- gauge.js
+++ gauge.js
@@ -1,9 +1,9 @@
-var gauge = Raphael('mydiv', 120, 200);
-var semicircle = gauge.circle(20, 100, 100).attr({"fill": "#9cf",'stroke-width': 0});
-var indicator = gauge.rect(15, 94, 8, 90,5).attr({fill: '#FFFFFF','stroke-width': 0});
-indicator.animate({"transform": "r-90,19,100"}, 2000, "bounce");
 
 function GaugeCtrl($scope){
+    var gauge = Raphael('mydiv', 120, 200);
+    var semicircle = gauge.circle(20, 100, 100).attr({"fill": "#9cf",'stroke-width': 0});
+    var indicator = gauge.rect(15, 94, 8, 90,5).attr({fill: '#FFFFFF','stroke-width': 0});
+    indicator.animate({"transform": "r-90,19,100"}, 2000, "bounce");
     
     $scope.setLitres = function(litres){
         indicator.animate({"transform": "r-"+litres+",19,100"}, 2000, "bounce");
